fe4f17cd88449532fd10c888c79afd39f4a83871
fladi06 <hl9306@naver.com>
2021-10-03 17:42:57 +0900

    - 아래 그림과 같이 출력하시오.
    \    /\
     )  ( ')
    (  /  )
     (__)|

---
+++ 소스.cpp
@@ -0,0 +1,23 @@
+/*
+ 아래 그림과 같이 출력하시오.
+
+\    /\
+ )  ( ')
+(  /  )
+ (__)|
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    /\\" << endl;
+	cout << ")  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << "(__)|" << endl;
+	cout << "*/" << endl;
+
+	return 0;
+}
